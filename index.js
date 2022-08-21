35c1b5a889c3208a7db0a0969bdbf3fc65ed6a5e
AshleyNicole1319 <a-warford96@comcast.net>
2022-08-21 16:50:51 -0400

    general outline for index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+//Array for Profiles
+const teamArray = [];
+
+//Consts for Profiles
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('..lib/Manager');
+
+
+
+//Prompts for Employees
+
+//Prompts for Manager
+
+//Prompts for Engineer
+
+//Prompts for Intern
+
